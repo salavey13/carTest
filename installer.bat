4c3be153a063e67d5d43a600ea640bee4dfa4019
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-02-04 05:06:04 +0300

    Update installer.bat
    typo:)

--- installer.bat
+++ installer.bat
@@ -241,37 +241,9 @@ echo Очистка временных файлов...
 del /q "%LATEST_ZIP_PATH%"
 call :ProgressBar 15 15
 
-@echo off
-:: Check for administrative privileges
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    echo Запрашиваю права администратора...
-    goto :UACPrompt
-) else ( goto :AdminAccess )
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    exit /B
-:AdminAccess
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
 
-:: ASCII Art (Razor Scene Group Style with SALAVEY13)
-echo.
-echo     ________   ________   ________   ________   ________  
-echo    /_______/  /_______/  /_______/  /_______/  /_______/  
-echo    |  ___  |  |  ___  |  |  ___  |  |  ___  |  |  ___  |  
-echo    | /   \ |  | /   \ |  | /   \ |  | /   \ |  | /   \ |  
-echo    |/_____\|  |/_____\|  |/_____\|  |/_____\|  |/_____\|  
-echo    SALAVEY13 SALAVEY13 SALAVEY13 SALAVEY13 SALAVEY13      
-echo.
-echo ============================================
-echo       Автоматизированный Установщик by Qwen
-echo ============================================
 
-:: Configuration
+:: Telegram Configuration
 set TELEGRAM_BOT_TOKEN=
 set ADMIN_CHAT_ID=
 
