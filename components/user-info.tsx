19acd464cb54565993e12768d0b49fb61beac950
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-03-02 02:33:09 +0300

    Update user-info.tsx

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -96,7 +96,7 @@ export default function UserInfo() {
       </span>
 
       {/* Modal */}
-      {isModalOpen && (
+      {isModalOpen && false && (
         <motion.div
           className="fixed inset-0 top-[69%] z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm"
           initial={{ opacity: 0 }}
