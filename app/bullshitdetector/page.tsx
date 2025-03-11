3e16d14c96860a05ac995ab009e3106277ae41ce
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-03-12 00:57:38 +0300

    Create page.tsx

---
+++ app/bullshitdetector/page.tsx
@@ -0,0 +1,7 @@
+import BullshitDetector from "@/components/BullshitDetector"; // Adjust path as needed
+
+export default function noHome4Bullshit() {
+  return     
+      <BullshitDetector />    
+  );
+}
