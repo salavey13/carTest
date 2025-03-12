6ba565353c5099552aea0b44be423b629523bb4c
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-03-13 00:12:49 +0300

    Create page.tsx

---
+++ app/wheel-of-fortune/page.tsx
@@ -0,0 +1,10 @@
+import WheelOfFortune from "@/components/WheelOfFortune"
+
+export default function WheelOfFortunePage() {
+  return (
+    <div className="min-h-screen py-12">
+      <WheelOfFortune />
+    </div>
+  )
+}
+
