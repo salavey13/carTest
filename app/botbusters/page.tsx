ce4fee978f4d4ca546524a0fc1caff92bc6cd4d5
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-03-15 19:51:25 +0300

    Update page.tsx

--- app/botbusters/page.tsx
+++ app/botbusters/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTelegram } from "@/hooks/useTelegram";
 import { supabaseAdmin, createAuthenticatedClient } from "@/hooks/supabase";
 import { Button } from "@/components/ui/button";
