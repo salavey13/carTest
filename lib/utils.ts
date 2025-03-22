f8fc07b1c55f0998fa7a1269f1afd82b7e219f81
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-03-23 01:24:42 +0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function getBaseUrl() {
+    return process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "https://v0-car-test.vercel.app";
