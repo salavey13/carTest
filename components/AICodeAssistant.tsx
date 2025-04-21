05fbedde484148851546dc707b9c9607bb785de6
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-04-21 04:15:15 +0300

    Update AICodeAssistant.tsx
    added useMemo

--- components/AICodeAssistant.tsx
+++ components/AICodeAssistant.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useImperativeHandle, forwardRef, MutableRefObject, useCallback } from "react";
+import React, { useMemo, useState, useEffect, useImperativeHandle, forwardRef, MutableRefObject, useCallback } from "react";
 // Context & Actions
 import {
     useRepoXmlPageContext, AICodeAssistantRef, SimplePullRequest
