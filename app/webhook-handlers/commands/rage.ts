b7e142c268c54b7fb20f71bae175e169cff7f76b
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-07-04 18:48:34 +0300

    Update rage.ts

--- app/webhook-handlers/commands/rage.ts
+++ app/webhook-handlers/commands/rage.ts
@@ -2,6 +2,6 @@ import { sendTelegramMessage } from "@/app/actions";
 import { logger } from "@/lib/logger";
 
 export async function rageCommand(chatId: number, userId: number) {
- logger.info([Rage Command] User ${userId} triggered the /rage command.);
+ logger.info(`[Rage Command] User ${userId} triggered the /rage command.`);
  await sendTelegramMessage("Rage command received! Opportunities check coming soon...", [], undefined, chatId.toString());
 }
