d85b3d09b2bae9690d3bf419f274dafcc5b99f63
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-08-18 03:12:36 +0300

    GROK4
    Apply AI assistant changes to 1 files.
    Ref: GROK4...

--- app/optimapipe/cases/[slug]/page.tsx
+++ app/optimapipe/cases/[slug]/page.tsx
@@ -3,6 +3,7 @@
 
 
 import React from "react";
+import { notFound } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
