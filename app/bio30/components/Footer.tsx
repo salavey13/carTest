18662a54ec24276312a404a9c2daf5e7967cb6f3
salavey13 <135398606+salavey13@users.noreply.github.com>
2025-11-06 01:21:56 +0300

    **[Проверяю внимательно, словно дифф между старым верстальщиком и совр
    Apply AI assistant changes to 1 files.
    Ref: **[Проверяю внимательно, словно дифф между старым верстальщиком и современным motion-художником]**...

--- app/bio30/components/Footer.tsx
+++ app/bio30/components/Footer.tsx
@@ -2,87 +2,171 @@
 
 import React from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 import { useBio30ThemeFix } from "../hooks/useBio30ThemeFix";
+import { useStaggerFadeIn } from "../hooks/useStaggerFadeIn";
 
 const Footer: React.FC = () => {
-  const docBase = "https://bio30.ru/docs";
   useBio30ThemeFix();
 
+  const { ref, controls, container, child } = useStaggerFadeIn(20, 0.08);
+  const docBase = "https://bio30.ru/docs";
+
   return (
-    <footer data-anim="fade" data-delay="0.1">
+    <motion.footer
+      ref={ref}
+      variants={container}
+      initial="hidden"
+      animate={controls}
+      className="footer"
+    >
       <div className="row ctr pd pd__xxl--top pd__xxl--btm">
-        <div className="row">
-          <a href="mailto:privet@bio30.ru" className="btn btn--blk btn__primary" data-anim="lux-up" data-delay="0.1">
+        <motion.div variants={child} className="row">
+          <a
+            href="mailto:privet@bio30.ru"
+            className="btn btn--blk btn__primary"
+          >
             privet@bio30.ru
           </a>
-        </div>
-        <div className="row ctr rgt">
+        </motion.div>
+
+        <motion.div variants={child} className="row ctr rgt">
           <div className="social gp gp--lg mg mg__lg--rgt">
-            <a href="https://t.me/BIO30_chat" className="telegram btn btn--blk btn__primary" data-anim="lux-up" data-delay="0.2"></a>
-            <a href="https://vk.com/club231438011" className="vk btn btn--blk btn__primary" data-anim="lux-up" data-delay="0.3"></a>
-            <a href="https://dzen.ru/id/6868db59568f80115b12a631" className="dzen btn btn--blk btn__primary" data-anim="lux-up" data-delay="0.4"></a>
+            <motion.a
+              variants={child}
+              href="https://t.me/BIO30_chat"
+              className="telegram btn btn--blk btn__primary"
+            />
+            <motion.a
+              variants={child}
+              href="https://vk.com/club231438011"
+              className="vk btn btn--blk btn__primary"
+            />
+            <motion.a
+              variants={child}
+              href="https://dzen.ru/id/6868db59568f80115b12a631"
+              className="dzen btn btn--blk btn__primary"
+            />
           </div>
-          <button id="language-btn" className="btn btn--blk btn__primary" aria-haspopup="true" aria-expanded="false" data-anim="lux-up" data-delay="0.5">
+          <motion.button
+            variants={child}
+            id="language-btn"
+            className="btn btn--blk btn__primary"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
             <span className="language"></span>
             <b className="link">Россия</b>
-          </button>
-        </div>
+          </motion.button>
+        </motion.div>
       </div>
+
       <div className="row">
-        <div className="col gp gp--md">
-          <span className="title fs__md fw__bd mg mg__md--btm" data-anim="lux-up" data-delay="0.1">КУПИТЬ</span>
-        </div>
-        <div className="col gp gp--md">
-          <span className="title fs__md fw__bd mg mg__md--btm" data-anim="lux-up" data-delay="0.1">РЕГИОН</span>
+        {/* КУПИТЬ */}
+        <motion.div variants={child} className="col gp gp--md">
+          <span className="title fs__md fw__bd mg mg__md--btm">КУПИТЬ</span>
+        </motion.div>
+
+        {/* РЕГИОН */}
+        <motion.div variants={child} className="col gp gp--md">
+          <span className="title fs__md fw__bd mg mg__md--btm">РЕГИОН</span>
           <div className="region-block col gp gp--xs">
-            <span className="link s__md fw__rg" data-anim="lux-up" data-delay="0.2">Россия</span>
+            <span className="link s__md fw__rg">Россия</span>
             <div id="languages-ru" className="languages-dropdown row gp gp--xs">
-              <a href="#" className="languages__item link fs__md fw__rg opc opc--50" data-lang="ru" data-region="ru" data-anim="fade" data-delay="0.28">
+              <motion.a
+                variants={child}
+                href="#"
+                className="languages__item link fs__md fw__rg opc opc--50"
+                data-lang="ru"
+                data-region="ru"
+              >
                 <span className="flag ru"></span>
                 Русский
-              </a>
-              <a href="#" className="languages__item link fs__md fw__rg opc opc--50" data-lang="en" data-region="ru" data-anim="fade" data-delay="0.48">
+              </motion.a>
+              <motion.a
+                variants={child}
+                href="#"
+                className="languages__item link fs__md fw__rg opc opc--50"
+                data-lang="en"
+                data-region="ru"
+              >
                 <span className="flag en"></span>
                 English
-              </a>
+              </motion.a>
             </div>
           </div>
-        </div>
-        <div className="col gp gp--md">
-          <span className="title fs__md fw__bd mg mg__md--btm" data-anim="lux-up" data-delay="0.1">ЗАРАБОТАТЬ</span>
-          <Link href="/bio30/referal" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.1">Общая информация</Link>
-          <Link href="/profile" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.1">Мой кабинет</Link>
-        </div>
-        <div className="col gp gp--md">
-          <span className="title fs__md fw__bd mg mg__md--btm" data-anim="lux-up" data-delay="0.1">ДОКУМЕНТЫ</span>
-          <a href="/docs/data" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.2"></a>
-          <a href="/docs/gdpr" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.3"></a>
-          <a href="/docs/confidencial" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.4"></a>
-          <a href="/docs/policy" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.5"></a>
-          <a href="/docs/info" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.6"></a>
-          <a href="/docs/payment" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.7"></a>
-          <a href="/docs/returns" className="link fs__md fw__rg opc opc--50 anmt" data-anim="lux-up" data-delay="0.8"></a>
-        </div>
+        </motion.div>
+
+        {/* ЗАРАБОТАТЬ */}
+        <motion.div variants={child} className="col gp gp--md">
+          <span className="title fs__md fw__bd mg mg__md--btm">ЗАРАБОТАТЬ</span>
+          <Link
+            href="/bio30/referal"
+            className="link fs__md fw__rg opc opc--50 anmt"
+          >
+            Общая информация
+          </Link>
+          <Link
+            href="/profile"
+            className="link fs__md fw__rg opc opc--50 anmt"
+          >
+            Мой кабинет
+          </Link>
+        </motion.div>
+
+        {/* ДОКУМЕНТЫ */}
+        <motion.div variants={child} className="col gp gp--md">
+          <span className="title fs__md fw__bd mg mg__md--btm">ДОКУМЕНТЫ</span>
+          {[
+            "data",
+            "gdpr",
+            "confidencial",
+            "policy",
+            "info",
+            "payment",
+            "returns",
+          ].map((slug, i) => (
+            <motion.a
+              key={slug}
+              variants={child}
+              href={`${docBase}/${slug}`}
+              className="link fs__md fw__rg opc opc--50 anmt"
+            >
+              {slug.toUpperCase()}
+            </motion.a>
+          ))}
+        </motion.div>
       </div>
+
       <div className="row top pd pd__xxl--top pd__xxl--btm">
-        <div className="aside">
+        <motion.div variants={child} className="aside">
           <div className="row ctr gp gp--xs">
             <div className="age"></div>
             <span className="text fs__sm fw__rg opc opc--50"></span>
           </div>
-        </div>
-        <div className="bside">
+        </motion.div>
+        <motion.div variants={child} className="bside">
           <div className="row ctr rgt gp gp--xs pd pd__lg--rgt">
             <span className="subtitle fs__sm fw__rg">
-              <a href="/docs/data" className="subtitle fs__sm fw__rg">Обработка персональных данных</a>
+              <a
+                href={`${docBase}/data`}
+                className="subtitle fs__sm fw__rg"
+              >
+                Обработка персональных данных
+              </a>
             </span>
             <span className="subtitle fs__sm fw__rg">
-              <a href="/docs/confidencial" className="subtitle fs__sm fw__rg">Политика конфиденциальности</a>
+              <a
+                href={`${docBase}/confidencial`}
+                className="subtitle fs__sm fw__rg"
+              >
+                Политика конфиденциальности
+              </a>
             </span>
           </div>
-        </div>
+        </motion.div>
       </div>
-    </footer>
+    </motion.footer>
   );
 };
 
