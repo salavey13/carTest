fc2db4b0c86935202bfadccd924480bb6fb5e888
salavey13 <135398606+salavey13@users.noreply.github.com>
2026-03-09 22:40:22 +0300

    Create mindmap.ts

---
+++ system/mindmap.ts
@@ -0,0 +1,12 @@
+export const cores = [
+  "core/events",
+  "core/garden",
+  "core/evolution",
+  "core/registry"
+]
+
+export const plugins = [
+  "app/greenbox",
+  "app/franchize",
+  "app/strikeball"
+]
