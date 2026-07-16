8ac6d77d366888259ab8337e7fe3b4aa78973e1a
fk-pasha-admin <pasha-admin@vip-bike.ru>
2026-07-16 18:14:10 +0000

    replace favicon.svg → favicon.png (compressed icon)

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,8 +44,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta charSet="utf-8" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="icon" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         <link rel="manifest" href="/manifest.json" /> 
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
         {/* Updated msapplication-TileColor to match light theme background by default */}
